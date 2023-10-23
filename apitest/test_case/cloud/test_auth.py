983a4713eabb99c559162833d8f44c0c7356a310
zll123123e <2806341129@qq.com>
2023-10-23 18:35:16 +0800

    新增获取个人认证链接页面接口

--- apitest/test_case/cloud/test_auth.py
+++ apitest/test_case/cloud/test_auth.py
@@ -8,6 +8,19 @@ from apitest.util.operate_yaml import read_case_yaml
 
 
 class TestAuth:
+
+    # 获取个人实名认证链接
+    @pytest.mark.testapi
+    @pytest.mark.parametrize(
+        "new_case_info",
+        read_case_yaml(
+            os.path.join(rootpath, "")
+        ),
+    )
+    def test_person_authurl(self, new_case_info):
+        request_Util().analyse_yaml(new_case_info)
+
+   #上传证件照图片
     @pytest.mark.testapi
     @pytest.mark.parametrize(
         "new_case_info",
@@ -15,7 +28,7 @@ class TestAuth:
             os.path.join(rootpath, "test_data/cloud/auth/person_card_image.yaml")
         ),
     )
-    def test_create_company(self, new_case_info):
+    def test_upload_person_image(self, new_case_info):
         request_Util().analyse_yaml(new_case_info)
 
 
